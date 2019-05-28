681f9178a25072ec763a4c947e1ac00aee99c006
bluesky1619 <duypase61594@fpt.edu.vn>
2019-05-28 22:23:51 +0700

    testing app

---
+++ WebApplication2/Controllers/CompaniesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApplication2;
+
+namespace WebApplication2.Controllers
+{
+    public class CompaniesController : ApiController
+    {
+        private swdEntities db = new swdEntities();
+
+        // GET: api/Companies
+        public IQueryable<Company> GetCompanies()
+        {
+            return db.Companies;
+        }
+
+        // GET: api/Companies/5
+        [ResponseType(typeof(Company))]
+        public IHttpActionResult GetCompany(int id)
+        {
+            Company company = db.Companies.Find(id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(company);
+        }
+
+        // PUT: api/Companies/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutCompany(int id, Company company)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != company.comID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(company).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CompanyExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Companies
+        [ResponseType(typeof(Company))]
+        public IHttpActionResult PostCompany(Company company)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Companies.Add(company);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (CompanyExists(company.comID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = company.comID }, company);
+        }
+
+        // DELETE: api/Companies/5
+        [ResponseType(typeof(Company))]
+        public IHttpActionResult DeleteCompany(int id)
+        {
+            Company company = db.Companies.Find(id);
+            if (company == null)
+            {
+                return NotFound();
+            }
+
+            db.Companies.Remove(company);
+            db.SaveChanges();
+
+            return Ok(company);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool CompanyExists(int id)
+        {
+            return db.Companies.Count(e => e.comID == id) > 0;
+        }
+    }
+}
